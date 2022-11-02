1a9992e8727dff2452cf16d8fc61d9e5f2f4818b
carlosbalbora <carlosb@albora.io>
2022-11-02 18:33:31 +0100

    Step 6 -  as a dynamic language, we consider incorrect cases
    Signed-off-by: carlosbalbora <carlosb@albora.io>

--- fizzbuzz/elixir/test/fizzbuzz_test.exs
+++ fizzbuzz/elixir/test/fizzbuzz_test.exs
@@ -5,5 +5,9 @@ defmodule FizzbuzzTest do
     test "return same value when is not multiple for 3 neither 5" do
       assert Fizzbuzz.play_number(1) == "1"
     end
+
+    test "fails when is not a number", state do
+      assert Fizzbuzz.play_number("a") == "a"
+    end
   end
 end
